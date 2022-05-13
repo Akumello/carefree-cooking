3c1dd4b0139d55a4edc656995005592f616cae78
Michael Gallahan <mgallaha@gmu.edu>
2022-05-12 19:02:31 -0700

    Initial commit

---
+++ src/main/java/com/michaelgallahancs/carefree_cooking/CarefreeCookingApplication.java
@@ -0,0 +1,13 @@
+package com.michaelgallahancs.carefree_cooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarefreeCookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarefreeCookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelgallahancs/carefree_cooking/CarefreeCookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelgallahancs.carefree_cooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarefreeCookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
