40cc29a4affb47b9981f4901bdcd12b3848e3aaf
Michael Gallahan <gallahanm@gmail.com>
2022-09-26 17:42:43 -0700

    Added the ability to use bullets or numbers and a few bugs fixed

--- carefree-ui/add_recipe.js
+++ carefree-ui/add_recipe.js
@@ -1,21 +1,35 @@
+/***** Data to collect from user *****/
+let recipeName = 'Mexican Rice';
+let ingredientList = ['Jasmine Rice', 'Tomato', 'Onion'];
+let instructionList = ['Blend onion, garlic, tomatoes, salt, and chicken boullion', 'Toast rice in oil over medium heat for about 7 mins until slightly golden.', 'Added blended mixture to toasted rice', 'Bring to a boil. Give one last stir, reduce heat to low, cover with lid and simmer for 15 mins.', 'Fluff rice and let rest for another 10 minutes before serving.'];
+/*************************************/
+
+/***** HTML Elements *****/
+let recipeNameInput = document.querySelector('#recipe-name');
+
 let ingredientListElem = document.querySelector('#ingredient-list');
 let addIngredientTextArea = document.querySelector('#add-ingredient-input');
 let addIngredientButton = document.querySelector('#add-ingredient-button');
-let ingredientList = ['Jasmine Rice', 'Tomato', 'Onion'];
 
 let instructionListElem = document.querySelector('#instruction-list');
 let addInstructionTextArea = document.querySelector('#add-instruction-input');
 let addInstructionButton = document.querySelector('#add-instruction-button');
-let instructionList = ['Blend onion, garlic, tomatoes, salt, and chicken boullion', 'Toast rice in oil over medium heat for about 7 mins until slightly golden.', 'Added blended mixture to toasted rice', 'Bring to a boil. Give one last stir, reduce heat to low, cover with lid and simmer for 15 mins.', 'Fluff rice and let rest for another 10 minutes before serving.'];
+/*************************/
 
-// createListItem(content)
-// generate html li element with text and return
-function createListItem(text, id)
+// Generate html li element for the list
+function createListItem(text, id, isBulleted)
 {
     let li = document.createElement('li');
     li.setAttribute('class', 'list-group-item d-flex justify-content-between');
     li.setAttribute('id', `li-${id}`);
-    li.innerHTML = `<div>${Number(id)+1}. ${text}</div><div class="d-none">${Number(id)+1}. <input type="text" value="${text}" id="input-${id}" size="65"></input></div><div style="cursor: pointer;"><i class="fas fa-edit p-1"></i><i class="fas fa-save p-1 px-2 d-none"></i><i class="fas fa-trash-alt p-1"></i></div>`;
+
+    let liBullet = String(Number(id)+1) + ".";
+    if(isBulleted)
+    {
+        liBullet = "&#8226;";
+    }
+
+    li.innerHTML = `<div>${liBullet} ${text}</div><div class="d-none">${liBullet} <input type="text" value="${text}" id="input-${id}" size="65"></input></div><div style="cursor: pointer;"><i class="fas fa-edit p-1"></i><i class="fas fa-save p-1 px-2 d-none"></i><i class="fas fa-trash-alt p-1"></i></div>`;
         /*
             <div>${Number(id)+1}. ${text}</div>
             <div class="d-none">
@@ -43,15 +57,20 @@ function updateListElement(list, listHtmlElem)
     }
     //*/
 
-    let newList = [...list];
+    let isBulleted = true; // Assume numbered list for new list types
+    if(listHtmlElem.id == 'ingredient-list')
+        isBulleted = true;
+    else if(listHtmlElem.id == 'instruction-list')
+        isBulleted = false;
 
+    let newList = [...list];
     // Display the todo list
     let i = 0;
-    console.log(list);
     list.forEach(item => {
         console.log(item);
-        if(item === newList[0]) {
-            listHtmlElem.appendChild(createListItem(item, `${i}`));
+        if(item === newList[0])
+        {
+            listHtmlElem.appendChild(createListItem(item, `${i}`, isBulleted));
             newList.shift();
         }
         i++;
@@ -119,15 +138,24 @@ function saveItem(clicked, list, listHtmlElem)
     updateListElement(list, listHtmlElem);
 }
 
-function editItem(clicked)
+function editItem(clicked, list, listHtmlElem)
 {
 
 
     // Switch the sibling div to an input text field and change edit button to a save button
     clicked.classList.add('d-none');
     clicked.nextSibling.classList.remove('d-none');
-    clicked.parentNode.parentNode.firstChild.nextSibling.classList.remove('d-none');;
+    clicked.parentNode.parentNode.firstChild.nextSibling.classList.remove('d-none');
     clicked.parentNode.parentNode.firstChild.setAttribute('class', 'd-none');
+
+    clicked.parentNode.parentNode.firstChild.nextSibling.firstChild.nextSibling.select();
+    clicked.parentNode.parentNode.firstChild.nextSibling.firstChild.nextSibling.addEventListener('keyup', e => {
+            if(e.keyCode === 13) 
+            {
+                saveItem(clicked, list, listHtmlElem);
+            }
+        }
+    );
 }
 
 //######## Save, Edit, and Delete button functionality ########
@@ -143,7 +171,7 @@ ingredientListElem.addEventListener('click', e =>
 
     // Edit button
     if(e.target.classList.contains('fa-edit'))
-        editItem(e.target);
+        editItem(e.target, ingredientList, ingredientListElem);
 });
 
 instructionListElem.addEventListener('click', e => 
@@ -158,7 +186,7 @@ instructionListElem.addEventListener('click', e =>
 
     // Edit button
     if(e.target.classList.contains('fa-edit'))
-        editItem(e.target);
+        editItem(e.target, instructionList, instructionListElem);
 });
 
 function generateJSON()
