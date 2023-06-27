e8b679f4eac8c7f90cb094b7ad041354bc4379b4
Michael Gallahan <gallahanm@gmail.com>
2023-06-26 19:26:11 -0700

    stash

--- carefree-ui/header_fade.js
+++ carefree-ui/header_fade.js
@@ -1,10 +1,10 @@
 let header = document.getElementById('website-title');
 
-function test() {
+document.body.addEventListener('scroll', e => {
   if(window.scrollY > header.offsetHeight) {
-      header.style.opacity = 0;
+    header.style.opacity = 0;
   }
   else if(window.scrollY < (header.offsetHeight/2)) {
       header.style.opacity = 1;
   }
-}
+});
